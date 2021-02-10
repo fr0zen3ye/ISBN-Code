4e826ec0aad2140bed08cee2f7fa59c822ff6221
Stefan Golker <stefangolker@Stefans-MBP.fritz.box>
2021-02-10 12:08:05 +0100

    fullfills task a

--- src/isbn.js
+++ src/isbn.js
@@ -1,20 +1,20 @@
 function checkISBN(digits) {
-    let stringSplit = digits.replace(/-/g, "");
+    let stringSplit = digits.split("-").join(""); 
     let factor = 1;
     let sum = 0;
 
-
     for (let i = 0; i < stringSplit.length; i++) {
-        sum = sum + factor * parseInt(stringSplit[i]);
+        parsedString = parseInt(stringSplit[i])
+        sum = sum + factor * parsedString;
         factor += 1;
     }
-    let checkdigit = sum % 11;
 
+    let checkdigit = sum % 11;
     if (checkdigit === 10){
         return "X";
     }
 
-    else if (checkdigit === 0) {
+    else if (checkdigit == 0) {
         return "";
     }
     return checkdigit.toString();
--- src/isbn.test.js
+++ src/isbn.test.js
@@ -1,16 +1,27 @@
 const {checkISBN} = require('./isbn');
 console.log(checkISBN);
 describe('checkISBN', () => {
-    it('should return X for 123456789', () => {
-        expect(checkISBN("123456789")).toBe("X");
-    })
-
     it('should return 3 for 123', () => {
-        expect(checkISBN("1-2-3")).toBe(3);
+        expect(checkISBN("1-2-3")).toBe("3");
     })
 
     it('should return 3 for 1---2----3', () => {
-        expect(checkISBN("1---2--3")).toBe(3);
+        expect(checkISBN("1---2--3")).toBe("3");
+    })
+    
+    it('should return X for 3-499-13599', () => {
+        expect(checkISBN("3-499-13599")).toBe("X");
+    })
+    it('should return X for 3-499-13599', () => {
+        expect(checkISBN("3--49-9----13599")).toBe("X");
     })
 
+    it('should return X for 3-7910-2163', () => {
+        expect(checkISBN("3-7910-2163")).toBe("X");
+    })
+   
+
+    it('should return "" for 3-928475-32', () => {
+        expect(checkISBN("3-928475-32")).toBe("");
+    })
 })
