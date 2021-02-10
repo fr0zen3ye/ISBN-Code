db70b583a02399296bde7eaee18fa9399f0d8525
Stefan Golker <stefangolker@Stefans-MBP.fritz.box>
2021-02-10 16:48:59 +0100

    fullfills task b

---
+++ src/script.js
@@ -0,0 +1,16 @@
+
+
+function checkUserInput(userISBN) {
+    
+    
+        var digits = document.getElementById("userISBN").value;
+        if (isISBNcorrect(digits) === true){
+        document.querySelector("#isbnCorrect").style.display = "block";
+        document.querySelector("#isbnFalse").style.display = "none";
+    }
+        else {
+        document.querySelector("#isbnFalse").style.display = "block";
+        document.querySelector("#isbnCorrect").style.display = "none";
+    }
+}
+
